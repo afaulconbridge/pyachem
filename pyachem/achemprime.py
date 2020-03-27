50707225fca45b95c5d359e74a1cda0b6c7625f5
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-03-27 21:23:13 +0000

    implement all_reactions

--- pyachem/achemprime.py
+++ pyachem/achemprime.py
@@ -8,6 +8,13 @@ from .reaction import Reaction
 class AChemPrime(AChem):
     """Example artificial chemistry based on prime numbers"""
 
+    def _react(self, a, b):
+        if b == a or b % a:
+            # same, or has a remainder, elastic reaction
+            return None
+        else:
+            return b // a
+
     def react(
         self, molecules: typing.Iterable[int], rng: random.Random
     ) -> Reaction[int]:
@@ -16,14 +23,21 @@ class AChemPrime(AChem):
         if rng.random() < 0.5:
             a, b = b, a
 
-        if b == a or b % a:
-            # same, or has a remainder, elastic reaction
+        c = self._react(a, b)
+        if not c:
             return None
         else:
-            c = b // a
             return Reaction((a, b), (a, c))
 
     def all_reactions(
         self, molecules: typing.Iterable[int]
     ) -> typing.Iterable[Reaction[int]]:
-        return super(AChemPrime, self).all_reactions(molecules)
+        a, b = molecules
+        reactants_all = [(a, b), (b, a)]
+        reactions = []
+        for reactants in reactants_all:
+            a, b = reactants
+            c = self._react(a, b)
+            if c:
+                reactions.append(Reaction((a, b), (a, c)))
+        return reactions
