8a3561301ce6c0b386ba4e8bbed0c49c8679e3b4
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-03-28 09:31:15 +0000

    make ReactionGraph iterable

--- tests/reactiongraph_test.py
+++ tests/reactiongraph_test.py
@@ -1,3 +1,5 @@
+import itertools
+
 import igraph
 from pyachem.achemprime import AChemPrime
 from pyachem.reactiongraph import ReactionGraph
@@ -5,6 +7,9 @@ from pyachem.reactiongraph import ReactionGraph
 
 def test_reactiongraph():
     reactiongraph = ReactionGraph(AChemPrime(), [2, 3, 4, 5, 6])
+
+    reactions = frozenset(reactiongraph)
+    assert len(reactions) == 3
 
     # layout = reactiongraph.graph.layout_kamada_kawai()
     # igraph.plot(reactiongraph.graph, layout = layout)
